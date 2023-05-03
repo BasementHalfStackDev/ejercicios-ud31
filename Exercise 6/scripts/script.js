a5da05b065e7a6eda4ba4f9ea8f83ece9bb9ebbb
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-05-03 13:28:01 +0200

    Exercise 6 finished

---
+++ Exercise 6/scripts/script.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Add listener to wait for content to load
+document.addEventListener("DOMContentLoaded", () => {
+    let clock = document.getElementById('clock');
+    // Apply listeners to all elements
+    updateClock();
+    setInterval(updateClock, 1000);
+});
